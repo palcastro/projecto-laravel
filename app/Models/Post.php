9f764ff36df833972d8e06146ed005f248624da7
paloma castro <91055754+palcastro@users.noreply.github.com>
2022-02-14 14:46:13 +0100

    primera prueba

---
+++ app/Models/Post.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post
+{
+    public $title; //Mi primer post = mi-primer-post
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+    }
+
+
+
+
+    public static function all()
+    {
+        return collect(File::files(resource_path("posts")))
+            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn ($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
+    }
+
+
+
+    public static function find($slug)
+    {
+        // base_path();
+        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+            throw new ModelNotFoundException();
+        }
+
+
+        return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
+    }
+}
